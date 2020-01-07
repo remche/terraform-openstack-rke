19989c95fcd5101717279775a9d19f5b9cc570b7
remche <remche@remche.org>
2020-01-07 22:06:23 +0100

    fix net name

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,5 +1,5 @@
 resource "openstack_networking_network_v2" "nodes_net" {
-  name           = "${var.neutron_cidr}-internal-net"
+  name           = "${var.cluster_name}-nodes-net"
   admin_state_up = "true"
   port_security_enabled = "false"
 }
