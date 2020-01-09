9715ec1ffd64fa2606dc0619d28b6044e10ac7a3
remche <remche@remche.org>
2020-01-09 15:14:22 +0100

    ressources dependencies (for clean destroy)

--- modules/network/output.tf
+++ modules/network/output.tf
@@ -1,3 +1,7 @@
+output "router_interface" {
+  value = openstack_networking_router_interface_v2.router_interface
+}
+
 output "nodes_net_name" {
   value = openstack_networking_network_v2.nodes_net.name
 }
