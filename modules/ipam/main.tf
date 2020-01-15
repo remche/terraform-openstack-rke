56a5dbed8d3b286814361452cb13e7db7abf604a
remche <remche@remche.org>
2020-01-15 17:07:47 +0100

    remove solidserver module

--- modules/ipam/main.tf
+++
@@ -1,7 +0,0 @@
-resource "solidserver_ip_address" "solid_address" {
-  count      = var.address_count
-  space      = var.solidserver_space
-  subnet     = var.solidserver_subnet
-  name       = "${var.dns_names[count.index]}.${var.dns_domain}"
-  request_ip = var.ips[count.index]
-}
