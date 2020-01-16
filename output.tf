79371db5a2abb0459097cd4a10ea758f95d3b86f
remche <remche@remche.org>
2020-01-16 16:21:56 +0100

    output rke cluster

--- output.tf
+++ output.tf
@@ -1,19 +1,25 @@
 output "keypair_name" {
-  value = module.keypair.keypair_name
+  value       = module.keypair.keypair_name
   description = "The name of the keypair used for nodes"
 }
 
 output "master_nodes" {
-  value = module.master.nodes
+  value       = module.master.nodes
   description = "The master nodes"
 }
 
 output "edge_nodes" {
-  value = module.worker.nodes
+  value       = module.worker.nodes
   description = "The edge nodes"
 }
 
 output "worker_nodes" {
-  value = module.worker.nodes
+  value       = module.worker.nodes
   description = "The worker nodes"
+}
+
+output "rke_cluster" {
+  value       = module.rke.rke_cluster
+  description = "RKE cluster spec"
+  sensitive   = "true"
 }
