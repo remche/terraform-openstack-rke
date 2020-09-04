1214943708389ad577d0d3e0bafda0a8426f4918
remche <remche@remche.org>
2020-09-04 12:18:12 +0200

    move node null_data_source to output.tf

--- modules/node/main.tf
+++ modules/node/main.tf
@@ -34,12 +34,3 @@ resource "openstack_compute_floatingip_associate_v2" "associate_floating_ip" {
   floating_ip = openstack_networking_floatingip_v2.floating_ip[count.index].address
   instance_id = openstack_compute_instance_v2.instance[count.index].id
 }
-
-data "null_data_source" "nodes" {
-  count = var.nodes_count
-  inputs = {
-    id          = openstack_compute_instance_v2.instance[count.index].id
-    internal_ip = openstack_compute_instance_v2.instance[count.index].access_ip_v4
-    floating_ip = var.assign_floating_ip ? openstack_networking_floatingip_v2.floating_ip[count.index].address : ""
-  }
-}
--- modules/node/output.tf
+++ modules/node/output.tf
@@ -2,6 +2,15 @@ output "associate_floating_ip" {
   value = openstack_compute_floatingip_associate_v2.associate_floating_ip[*]
 }
 
+data "null_data_source" "nodes" {
+  count = var.nodes_count
+  inputs = {
+    id          = openstack_compute_instance_v2.instance[count.index].id
+    internal_ip = openstack_compute_instance_v2.instance[count.index].access_ip_v4
+    floating_ip = var.assign_floating_ip ? openstack_networking_floatingip_v2.floating_ip[count.index].address : ""
+  }
+}
+
 output "nodes" {
   value = zipmap(openstack_compute_instance_v2.instance[*].name, data.null_data_source.nodes[*].outputs)
 }
