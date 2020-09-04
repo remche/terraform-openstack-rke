6ae637d04d3a5f4dd9e5db2c93745fd300785925
remche <remche@remche.org>
2020-09-04 13:00:31 +0200

    fix ca_crt output

--- modules/rke/output.tf
+++ modules/rke/output.tf
@@ -1,7 +1,7 @@
 data "null_data_source" "rke_cluster" {
   inputs             = {
     api_server_url   = rke_cluster.cluster.api_server_url
-    ca_cert          = rke_cluster.cluster.ca_crt
+    ca_crt          = rke_cluster.cluster.ca_crt
     client_cert      = rke_cluster.cluster.client_cert
     client_key       = rke_cluster.cluster.client_key
     kube_config_yaml = rke_cluster.cluster.kube_config_yaml
