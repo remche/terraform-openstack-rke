aad22e70359743eb7997c9d3d3d48c84d0f38d20
remche <remche@remche.org>
2020-09-04 12:38:58 +0200

    filter output to add only useful ones

--- modules/rke/output.tf
+++ modules/rke/output.tf
@@ -1,4 +1,15 @@
+data "null_data_source" "rke_cluster" {
+  inputs             = {
+    api_server_url   = rke_cluster.cluster.api_server_url
+    ca_cert          = rke_cluster.cluster.ca_crt
+    client_cert      = rke_cluster.cluster.client_cert
+    client_key       = rke_cluster.cluster.client_key
+    kube_config_yaml = rke_cluster.cluster.kube_config_yaml
+    rke_cluster_yaml = rke_cluster.cluster.rke_cluster_yaml
+  }
+}
+
 output "rke_cluster" {
-  value       = rke_cluster.cluster
+  value       = data.null_data_source.rke_cluster.outputs
   description = "RKE cluster spec"
 }
