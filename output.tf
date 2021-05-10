c9baa0cdf02720e1b878aea497046c687dd74140
remche <remche@remche.org>
2021-05-10 22:24:55 +0200

    mark all outpouts as sensitive

--- output.tf
+++ output.tf
@@ -1,21 +1,25 @@
 output "keypair_name" {
   value       = module.keypair.keypair_name
   description = "The name of the keypair used for nodes"
+  sensitive   = "true"
 }
 
 output "master_nodes" {
   value       = module.master.nodes
   description = "The master nodes"
+  sensitive   = "true"
 }
 
 output "edge_nodes" {
   value       = module.edge.nodes
   description = "The edge nodes"
+  sensitive   = "true"
 }
 
 output "worker_nodes" {
   value       = module.worker.nodes
   description = "The worker nodes"
+  sensitive   = "true"
 }
 
 output "rke_cluster" {
@@ -27,9 +31,11 @@ output "rke_cluster" {
 output "nodes_subnet" {
   value       = module.network.nodes_subnet
   description = "The nodes subnet"
+  sensitive   = "true"
 }
 
 output "loadbalancer_floating_ip" {
   value       = var.enable_loadbalancer ? module.loadbalancer[0].floating_ip : ""
   description = "The floating ip of the loadbalancer"
+  sensitive   = "true"
 }
